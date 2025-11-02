8064f46773c442801c86a1276c0b6c1e6d2ca936
Dagi Ameha <dagimameha@gmail.com>
2025-11-02 21:46:04 +0300

    first commit

---
+++ src/components/sections/ProjectsSection.tsx
@@ -0,0 +1,17 @@
+import { ProjectCard } from "@/components/ProjectCard";
+import { projects } from "public/data/project-detail";
+
+export function ProjectsSection() {
+  return (
+    <section id="projects" className="py-20">
+      <h2 className="text-4xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-white mb-8 pb-2">
+        Projects
+      </h2>
+      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 ">
+        {projects.map((p) => (
+          <ProjectCard key={p.title} {...p} />
+        ))}
+      </div>
+    </section>
+  );
+}
